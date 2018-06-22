48e753b5c7d9184208be111cc40b450ceaf33c91
heyunjie <30441342+heyunjie@users.noreply.github.com>
2018-06-22 11:35:47 +0800

    new code
    hello world

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+	/*
+	 * 程序的主入口
+	 */
+  public static void main(String[] args) {
+	  // 在控制台输出一段英文Hello world
+	  System.out.println("Hello world!");
+	  //打印并换行，机器人Hello world叫了一声Hello world
+  }
+}
